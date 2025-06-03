1a66abf0684677ab6e200004b88204393f62b56d
laboratorio <laboratorio@UPOAULA10723>
2025-06-03 07:17:04 -0500

    Agregar archivos de proyecto.

---
+++ ExamenProgresoMM/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ExamenProgresoMM;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
